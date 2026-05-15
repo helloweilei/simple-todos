be06728b611ae5d95a1639ea137368ea9af1e4f0
魏磊 <15718965+charlie_wei@user.noreply.gitee.com>
2026-05-15 17:49:24 +0800

    优化输入

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Text } from "ink";
+import React, { useEffect, useState } from "react";
+import { Box, Text } from "ink";
 
 interface Props {
   value: string;
 }
 
 export default function Prompt({ value }: Props) {
+  const [cursorOn, setCursorOn] = useState(true);
+
+  useEffect(() => {
+    const id = setInterval(() => setCursorOn((v) => !v), 530);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
   return (
-    <Text>
+    <Box flexDirection="row" flexWrap="nowrap">
       <Text bold>&gt; </Text>
-      {value}
-      <Text inverse> </Text>
-    </Text>
+      <Text>{value}</Text>
+      <Text inverse={cursorOn} dimColor={!cursorOn}>█</Text>
+    </Box>
   );
 }
